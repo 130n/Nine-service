7a65ed13d88abcd580080cb86b2b39cc19ec5682
Leon Hennings <leon.hennings@ciber.com>
2014-10-29 14:36:10 +0100

    NineController: added /nine/check.json which takes same input as /nine/validate.json but only returns {correct: boolean}

--- src/main/java/nine/NineController.java
+++ src/main/java/nine/NineController.java
@@ -10,6 +10,7 @@ import nine.pojo.Nine;
 import nine.pojo.NineDiff;
 import nine.pojo.NineResult;
 import nine.pojo.WordList;
+import pojo.NineCheck;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,13 +22,13 @@ public class NineController {
 
     private final NineSolver nineSolver = new NineSolver();
 
-    private final Map<String, Integer> cheatCounter = new HashMap<String, Integer>();
     /**
      * Generate nine random letters
      * @return JSON in format {"letters": "abcdefghi"}
      */
     @RequestMapping(value = "/nine/getnine.json", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public Nine getnine() {
+        System.out.println("GET /nine/getnine.json");
         return new Nine();
     }
 
@@ -42,6 +43,7 @@ public class NineController {
      */
     @RequestMapping(value = "/nine/wordlist.json", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public WordList wordlist() {
+        System.out.println("GET /nine/wordlist.json");
         return WordList.SAOL;
     }
 
@@ -51,12 +53,6 @@ public class NineController {
         if(input.getNineLetters().length() != 9){
             return new ResponseEntity<NineDiff>(HttpStatus.BAD_REQUEST);
         }
-        if (cheatCounter.get(input.getNineLetters()) == null ){
-            cheatCounter.put(input.getNineLetters(), 1);
-        } else {
-            Integer counter = cheatCounter.get(input.getNineLetters());
-            cheatCounter.put(input.getNineLetters(), ++counter);
-        }
 
         String mustLetter = "" + input.getNineLetters().charAt(4);
         String possLetters = input.getNineLetters().substring(0,4) + input.getNineLetters().substring(5,9);
@@ -64,6 +60,22 @@ public class NineController {
         System.out.println(possLetters);
         List<String> facit = nineSolver.getFacit(mustLetter, possLetters);
         return new ResponseEntity<NineDiff>(new NineDiff(input, facit),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/nine/check.json", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody ResponseEntity<NineCheck> check(@RequestBody NineResult input) {
+        System.out.println(ReflectionToStringBuilder.toString(input));
+        if(input.getNineLetters().length() != 9){
+            return new ResponseEntity<NineCheck>(HttpStatus.BAD_REQUEST);
+        }
+
+        String mustLetter = "" + input.getNineLetters().charAt(4);
+        String possLetters = input.getNineLetters().substring(0,4) + input.getNineLetters().substring(5,9);
+        System.out.println(mustLetter);
+        System.out.println(possLetters);
+
+        List<String> facit = nineSolver.getFacit(mustLetter, possLetters);
+        return new ResponseEntity<NineCheck>(new NineCheck(input, facit),HttpStatus.OK);
     }
 
     @RequestMapping(value = "/error")
--- src/main/java/nine/pojo/NineResult.java
+++ src/main/java/nine/pojo/NineResult.java
@@ -1,11 +1,12 @@
 package nine.pojo;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by LeoHen on 2014-10-17.
  */
-public class NineResult {
+public class NineResult implements Serializable {
 
     private String nineLetters;
     private List<String> result;
